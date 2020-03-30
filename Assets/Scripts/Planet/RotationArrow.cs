ba856c561e9f0ad045a2d871f288dd3af3caad63
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-03-30 15:02:13 +0200

    [Dev] Planet arrow rotation

---
+++ Assets/Scripts/Planet/RotationArrow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotationArrow : MonoBehaviour
+{
+    [SerializeField] private Vector3 _direction;
+    [SerializeField] private PlanetMovement _planet;
+
+    public void OnClick()
+    {
+        _planet.LaunchRotation(_direction);
+    }
+}
