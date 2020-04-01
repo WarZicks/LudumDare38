537fab5cbf9d7caf7e76747e0b1f8044eccf4769
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-04-01 12:01:47 +0200

    [Dev] Update camp mesh when step passed + new camps prefab

--- Assets/Scripts/CampMesh.cs
+++ Assets/Scripts/CampMesh.cs
@@ -4,6 +4,9 @@ public class CampMesh : MonoBehaviour
 {
     public void CreateNewMesh(GameObject mesh)
     {
+        foreach (Transform child in this.gameObject.transform)
+            Destroy(child.gameObject);
+
         GameObject go = Instantiate(mesh, this.transform);
         go.transform.localPosition = new Vector3(.03f, 0f, .045f);
     }
--- Assets/Scripts/Managers/CampManager.cs
+++ Assets/Scripts/Managers/CampManager.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class CampManager : MonoBehaviour
 {
     [Header("Camps")]
+    [SerializeField] private GameObject _initialCamp;
     [SerializeField] private GameObject _firstCamp;
     [SerializeField] private GameObject _secondCamp;
     [SerializeField] private GameObject _thirdCamp;
-    [SerializeField] private GameObject _fourthCamp;
 
     [Header("Camp Steps")]
     [SerializeField] private int _firstCampStep;
@@ -26,7 +26,54 @@ public class CampManager : MonoBehaviour
     {
         foreach(CampMesh camp in _campList)
         {
-            camp.CreateNewMesh(_firstCamp);
+            camp.CreateNewMesh(_initialCamp);
+        }
+
+        GameManager.instance.onUpdateEndWay += () => CheckCampStep();
+
+    }
+
+    private void CheckCampStep()
+    {
+        // FIRST CAMP STEP
+        if (GameManager.instance._endWayActivated >= _firstCampStep)
+        {
+            foreach (CampMesh camp in _campList)
+            {
+                camp.CreateNewMesh(_firstCamp);
+                // LAUNCH VFX & SFX
+            }
+        }
+        else
+        {
+            foreach (CampMesh camp in _campList)
+            {
+                camp.CreateNewMesh(_initialCamp);
+
+                // LAUNCH VFX & SFX
+            }
+        }
+
+        // SECOND CAMP STEP
+        if (GameManager.instance._endWayActivated >= _secondCampStep)
+        {
+            foreach (CampMesh camp in _campList)
+            {
+                camp.CreateNewMesh(_secondCamp);
+
+                // LAUNCH VFX & SFX
+            }
+        }
+
+        // THIRD CAMP STEP
+        if (GameManager.instance._endWayActivated >= _thirdCampStep)
+        {
+            foreach (CampMesh camp in _campList)
+            {
+                camp.CreateNewMesh(_thirdCamp);
+
+                // LAUNCH VFX & SFX
+            }
         }
     }
 }
