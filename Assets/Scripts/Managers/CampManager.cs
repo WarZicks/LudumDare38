57df2bb924592cf581117ef3d9fdc12ba2be480c
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-04-01 10:31:16 +0200

    [Int] Planet lvl 01 add camp + campfires folders - Camp holder int

---
+++ Assets/Scripts/Managers/CampManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CampManager : MonoBehaviour
+{
+    [Header("Camps")]
+    [SerializeField] private GameObject _firstCamp;
+    [SerializeField] private GameObject _secondCamp;
+    [SerializeField] private GameObject _thirdCamp;
+    [SerializeField] private GameObject _fourthCamp;
+
+    [Header("Camp Steps")]
+    [SerializeField] private int _firstCampStep;
+    [SerializeField] private int _secondCampStep;
+    [SerializeField] private int _thirdCampStep;
+
+    private List<CampMesh> _campList = new List<CampMesh>();
+}
