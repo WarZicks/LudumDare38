1087b66f8efd3f3de6fe124208182b7387037870
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-04-02 16:56:08 +0200

    [Int] don't destroy music between scenes + add planet turn soud + add music

---
+++ Assets/Scripts/MusicSingleton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSingleton : MonoBehaviour
+{
+    public static MusicSingleton instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
