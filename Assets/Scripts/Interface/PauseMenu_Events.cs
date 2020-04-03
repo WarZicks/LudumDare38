1a2f62a9b81431edcc04b3056021b5ef09eb653f
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-04-03 12:47:57 +0200

    [Dev] Pause Menu

---
+++ Assets/Scripts/Interface/PauseMenu_Events.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu_Events : MonoBehaviour
+{
+    public void OnClickResume()
+    {
+        InterfaceManager.instance.TogglePauseMenu();
+    }
+
+    public void OnClickMenu()
+    {
+        InterfaceManager.instance.GoToMenu();
+    }
+
+    public void OnClickQuit()
+    {
+        InterfaceManager.instance.Quit();
+    }
+}
